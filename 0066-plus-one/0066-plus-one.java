b0128175000f39a28bf8db2586c37a1f39db0fbf
NIKHIL SHARMA <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-09-23 18:32:23 +0530

    Time: 0 ms (100%), Space: 43 MB (97.24%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i= digits.length-1;
+        while(i>=0){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+            i--;
+        }
+        digits = new int[digits.length +1];
+        digits[0] = 1;
+    return digits;
+    }
+}
